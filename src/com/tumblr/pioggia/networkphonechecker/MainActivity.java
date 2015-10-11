e0e35bc49f883c967b77167c76820fd6ae3c2ef4
anhlht <anhlht@outlook.com>
2015-10-11 20:57:45 +0700

    activity provider

--- src/com/tumblr/pioggia/networkphonechecker/MainActivity.java
+++ src/com/tumblr/pioggia/networkphonechecker/MainActivity.java
@@ -1,12 +1,13 @@
 package com.tumblr.pioggia.networkphonechecker;
 
+import java.util.Currency;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,15 +15,16 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-	Button btnchondanhba;
+	Button btnChonDanhDa,btnChonNhaMang;
 	EditText etSDT;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		etSDT = (EditText)findViewById(R.id.et_sdt);
-		btnchondanhba = (Button)findViewById(R.id.btnchondanhba);
-		btnchondanhba.setOnClickListener(new View.OnClickListener() {
+		
+		btnChonDanhDa = (Button)findViewById(R.id.btnchondanhba);
+		btnChonDanhDa.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
@@ -31,7 +33,18 @@ public class MainActivity extends Activity {
 			}
 		});
 		
-		
+		btnChonNhaMang = (Button)findViewById(R.id.btnnhamang);
+		btnChonNhaMang.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Intent intent = new Intent(MainActivity.this,ProviderActivity.class);
+				startActivity(intent);
+				
+			}
+		});
 	}
 
 	@Override
